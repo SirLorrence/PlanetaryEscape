a24d12bcff9c1f3e0d14f783a07386d55b227087
Laurence Sadler <33729843+lvlzro@users.noreply.github.com>
2020-12-01 21:36:04 -0800

    basic fps controller added

---
+++ Planetary Escape/Assets/Scripts/PlayerScripts/FPS/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    private Vector3 movement;
+    private PlayerStats playerStats;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        playerStats = GetComponent<PlayerStats>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var x = Input.GetAxisRaw("Horizontal");
+        var y = Input.GetAxisRaw("Vertical");
+
+        movement = (x * transform.right + y * transform.forward).normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(transform.position + (movement * (playerStats.MovementSpeed * Time.deltaTime)));
+    }
+}
