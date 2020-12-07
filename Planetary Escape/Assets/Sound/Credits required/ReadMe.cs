2956349a2d1d2280ffaa3d5f6c225c9ecbdbbecd
CODman-2019 <55792105+CODman-2019@users.noreply.github.com>
2020-12-07 18:03:07 -0400

    sounds and music added

---
+++ Planetary Escape/Assets/Sound/Credits required/ReadMe.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadMe : MonoBehaviour
+{
+    /// The sounds in this file require creditation. I looked through the licenses of all the sounds and music and  if they require creditation
+    /// I will add them here based on the sounds
+    /// 
+    /// the 2 death sounds
+    ///                  https://freesound.org/people/MATRIXXX_/sounds/523772/
+    ///
+}
