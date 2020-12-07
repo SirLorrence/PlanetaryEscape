8031a711b20891fe1bc4093833c7ca8f4414c6d4
CODman-2019 <55792105+CODman-2019@users.noreply.github.com>
2020-12-07 18:06:13 -0400

    edited ReadMe file

--- Planetary Escape/Assets/Sound/ReadMe.cs
+++ Planetary Escape/Assets/Sound/ReadMe.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class ReadMe : MonoBehaviour
 {
     /// The sounds in this file require creditation. I looked through the licenses of all the sounds and music and  if they require creditation
-    /// I will add them here based on the sounds
+    /// I will add them here based on the sounds (just copy and paste)
     /// 
-    /// the 2 death sounds
+    /// the 2 game over sounds
     ///                  https://freesound.org/people/MATRIXXX_/sounds/523772/
+    ///                  
+    /// Deep coord ambience (for resutls screen)
+    ///                 https://freesound.org/people/Jedo/sounds/397007/
     ///
 }
