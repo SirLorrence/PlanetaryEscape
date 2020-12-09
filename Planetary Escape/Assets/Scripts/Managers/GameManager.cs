a6e3bfa1713cfbe51361bd62be7e1a26ac47d3bf
Lucas McCully <thecarguru2401@gmail.com>
2020-12-09 16:25:43 -0400

    Fixed UI bug freezing results before the screen appeared

--- Planetary Escape/Assets/Scripts/Managers/GameManager.cs
+++ Planetary Escape/Assets/Scripts/Managers/GameManager.cs
@@ -164,10 +164,8 @@ public class GameManager : MonoBehaviour
         //GameObject go = GameObject.FindGameObjectWithTag("Player");
         //go.GetComponentInChildren<Animator>().SetBool("isDead", true);
         InputManager.Instance.Freeze();
-        yield return new WaitForSeconds(2);
+        //yield return new WaitForSeconds(2);
         uiManager.ChangeScreen((int)UIManager.Screens.Results);
-        Time.timeScale = 0;
-        InputManager.Instance.Unfreeze();
         Debug.Log("Dead");
         yield return null;
     }
