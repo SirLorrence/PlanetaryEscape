00a4fa27bdf56a5696287e0c33019b991fc63060
Laurence Sadler <sirlaurence@protonmail.com>
2020-12-09 22:08:02 -0800

    bullets prefabs added, some fixes

--- Planetary Escape/Assets/Scripts/Bullet.cs
+++ Planetary Escape/Assets/Scripts/Bullet.cs
@@ -5,15 +5,11 @@ public class Bullet : MonoBehaviour
     [SerializeField] float bulletDuration = 5;
     [SerializeField] private string targetTag;
     [HideInInspector] public int damage = 1;
-    
 
     private float speed;
     private float time;
 
-    private void Start()
-    {
-        time = Time.time;
-    }
+    private void Start() => time = Time.time;
 
     public void OnStart(float speed, int damage)
     {
@@ -25,25 +21,14 @@ public class Bullet : MonoBehaviour
 
     private void Update()
     {
-        if (time + bulletDuration < Time.time)
-        {
-            GameManager.Instance.SetObjectInPool(gameObject);
-        }
+        if (time + bulletDuration < Time.time) GameManager.Instance.SetObjectInPool(gameObject);
 
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 
     void OnTriggerEnter(Collider c)
     {
-        if (c.gameObject.CompareTag(targetTag))
-            GameManager.Instance.TakeDamage(c.gameObject, damage);
-        else if (c.gameObject.CompareTag("Bullet"))
-        {
-            
-        }
-        else
-        {
+        if (c.gameObject.CompareTag(targetTag)) GameManager.Instance.TakeDamage(c.gameObject, damage);
             GameManager.Instance.SetObjectInPool(gameObject);
-        }
     }
 }
