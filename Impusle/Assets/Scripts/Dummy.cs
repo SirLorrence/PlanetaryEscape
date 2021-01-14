0900b015dd0ff1d684f896ea79d2dd46cb6736d3
Lucas McCully <thecarguru2401@gmail.com>
2021-01-14 15:28:13 -0400

    Added Local Networking and Player Movement + Shooting

---
+++ Impusle/Assets/Scripts/Dummy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dummy : MonoBehaviour
+{
+    public void TakeDamage(float damageAmount)
+    {
+        GameObject go = ObjectPooler.Instance.GetGameObject(0);
+        go.transform.position = gameObject.transform.position + new Vector3(0,2,0);
+        DamagePopup damagePopup = go.GetComponent<DamagePopup>();
+        damagePopup.gameObject.SetActive(true);
+        damagePopup.Setup((int)damageAmount);
+    }   
+}
---
+++ Impusle/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerShoot : NetworkBehaviour
+{
+    [Header("Assignable")] 
+    public Transform gunTipTransform;
+    public Transform cameraTransform;
+    public LayerMask shootingLayerMask;
+
+    public enum Guns{Pistol}
+
+    [Header("Debug")]
+    public Guns currentWeapon;
+
+
+    private float cooldown = 0.2f;
+    private float shootTimer = 0f;
+
+    void OnEnable()
+    {
+        shootTimer = Time.realtimeSinceStartup + cooldown;
+    }
+
+    public void Shoot()
+    {
+        print("Shoot");
+        if (shootTimer < Time.realtimeSinceStartup)
+        {
+            print("Able to Shoot");
+            shootTimer = Time.realtimeSinceStartup + cooldown;
+            Ray ray = new Ray(cameraTransform.position, cameraTransform.forward * 50);
+            if (Physics.Raycast(ray, out RaycastHit hit, 200, shootingLayerMask))
+            {
+                print("Successful Hit");
+                if (hit.collider.CompareTag("Enemy"))
+                {
+                    print("Deal damage");
+                    DamageableBodyPart dummy = hit.collider.gameObject.GetComponent<DamageableBodyPart>();
+                    dummy.TakeDamage(101);
+                    print(hit.collider.name);
+                }
+            }
+        }
+    }
+
+    public void ChangeWeapon()
+    {
+
+    }
+
+    void ShootBullet(Vector3 position, Vector3 rotation)
+    {
+        GameObject go = ObjectPooler.Instance.GetGameObject(1);
+        go.transform.position = position;
+        go.transform.LookAt(rotation);
+        go.SetActive(true);
+    }
+}
