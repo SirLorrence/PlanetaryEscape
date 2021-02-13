06b65bd28af53de8d427fd4d2d9edc1100fe82b0
Laurence Sadler <sirlaurence@protonmail.com>
2021-02-13 10:55:48 -0800

    ADS working

---
+++ Impulse/Assets/Scripts/Weapons/WeaponADS.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using UnityEngine;
+
+public class WeaponADS : MonoBehaviour
+{
+	public bool toggelAimSights;
+
+	public Transform Hip;
+	public Transform weapon;
+	public Transform Aim;
+
+	public float smooth;
+
+	private void Update() {
+		if (Input.GetMouseButton(1) || toggelAimSights) {
+			LerpPosition(Aim.position, smooth);
+		}
+		else LerpPosition(Hip.position, smooth);
+	}
+
+
+	void LerpPosition(Vector3 targetLoc, float duration) {
+			var startpos = weapon.position;
+			weapon.position = Vector3.Lerp(startpos, targetLoc,  duration * Time.deltaTime);
+			Debug.Log("Done");
+	}
+}
