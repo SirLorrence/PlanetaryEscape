3c3b58cc3e8569998a7e257a248f64f1095c8795
Lucas McCully <thecarguru2401@gmail.com>
2021-02-23 14:04:53 -0400

    Fixed Namespaces

--- Impulse/Assets/Scripts/Player/BodyAnimation.cs
+++ Impulse/Assets/Scripts/Player/BodyAnimation.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using UnityEditor.Il2Cpp;
 using UnityEngine;
 
 
--- Impulse/Assets/Scripts/Player/CameraFollow.cs
+++ Impulse/Assets/Scripts/Player/CameraFollow.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 using Unity.Mathematics;
-using UnityEditor.IMGUI.Controls;
 
 public class CameraFollow : NetworkBehaviour
 {
